1d463a414c9e1a74b6915897f96e83f62969a87b
Fangzhou Hu <f9hu@ucsd.edu>
2018-10-18 09:52:52 -0700

    commit -m "parsing url"

--- Section 3/app/index.js
+++ Section 3/app/index.js
@@ -4,15 +4,29 @@
  * 
  */
 
- //Deoendencies 
+ // Deoendencies 
  var http = require('http');
+ var url = require('url');
 
- //The server shoud respond to all request with a string 
+ // The server shoud respond to all request with a string 
  var server = http.createServer(function(req,res){
-    res.end("");
+    
+    // get URL and parse it (paese in true to call queryString)
+    let parseUrl = url.parse(req.url,true);
+
+    // get path of the URL
+    let path = parseUrl.pathname; //un trimed path 
+    let trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    // send response 
+    res.end('hello\n');
+
+
+    // Log the request path
+    console.log('Request received on path: '+trimmedPath);
+
  });
 
- //Start the server, and have it listen to port 3000
+ // Start the server, and have it listen to port 3000
  server.listen(3000,function(){
      console.log("Server is listening to port 3000");
  })
